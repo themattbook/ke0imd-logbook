c4d4d9a351efda43b9db22b45eca2417d8a53c0d
themattbook <themattbook@gmail.com>
2023-05-15 09:34:25 -0500

    Added vue-router and setup default Home route

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createRouter, createWebHistory } from 'vue-router';
 import './style.css';
 import App from './App.vue';
 
+import Home from './views/Home.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [{ path: '/', name: 'Home', component: Home }],
+});
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(router);
 app.use(pinia);
 app.mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Login from '../components/LoginUser.vue';
+import RegisterUser from '../components/RegisterUser.vue';
+import { ref } from 'vue';
+
+const newUser = ref(false);
+const toggleNewUser = () => {
+    newUser.value = !newUser.value;
+};
+</script>
+<template>
+    <div class="flex items-center justify-center h-[calc(100vh-66px)]">
+        <div class="hero h-full bg-base-200">
+            <div class="hero-content flex-col">
+                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <Login v-if="!newUser" />
+                    <RegisterUser v-if="newUser" />
+                </div>
+                <div>
+                    <p @click="toggleNewUser" class="cursor-pointer text-center">New user? Create an account</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
